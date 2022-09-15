7eb915d5c790cfc99123726f2630edfebe0ba140
Hyeon Woo Jo <haenu@haenu.com>
2022-09-15 13:50:52 +0900

    feat: 서비스 종료 안내 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme, Alert, AlertIcon, AlertDescription, AlertTitle } from '@chakra-ui/react';
 import Nav from './Components/Nav';
 import Content1 from './Components/Content1';
 import Content2 from './Components/Content2';
@@ -52,6 +52,11 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Nav />
+      <Alert status='error'>
+        <AlertIcon />
+        <AlertTitle>서비스 종료 안내</AlertTitle>
+        <AlertDescription>신규 방송알림봇 개편에 따라 기존 봇을 활용하던 방송정보 사이트는 운영을 중단하게 되었습니다. <a href='https://cafe.naver.com/leaven0402/2327' target='_blank'>관련 공지</a></AlertDescription>
+      </Alert>
       <Content1 data={{
         member
       }} />
